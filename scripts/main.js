b91ddf7d3e12226b64f468e7bf90cb08031fe812
Erich Hils <mrerichhils@gmail.com>
2022-01-12 11:50:12 -0600

    update fish class in function

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,10 @@ const allFish = getFish()
 for (const fish of allFish) {
     console.log(fish)
 }
+
+import { FishList } from './FishList.js'
+
+const parentHTMLElement = document.querySelector(".fishListContainer")
+
+
+parentHTMLElement.innerHTML = FishList()
