a45d7f9c265aed80a4fba5fe1b408c679e1aa2bd
Erich Hils <mrerichhils@gmail.com>
2022-01-12 10:48:44 -0600

    added list object/fish array

---
+++ scripts/database.js
@@ -0,0 +1,39 @@
+const database = {
+    fish: [
+        {
+            name: "Kevin",
+            species: "Puffer Fish",
+            length: 25,
+            origin: "Grand Cayman",
+            diet: "coral"
+        },
+        {
+            name: "Kyle",
+            species: "popper fish",
+            length: 13,
+            origin: "Israel",
+            diet: "sand"
+        },
+        {
+            name: "Lloyd",
+            species: "clown fish",
+            length: 4,
+            origin: "Australia",
+            diet: "coral"
+        },
+        {
+            name: "cynthia",
+            species: "StarFish",
+            length: 15,
+            origin: "Portugal",
+            diet: "bacteria"
+        },
+        {
+            name: "Toad",
+            species: "frog fish",
+            length: 7,
+            origin: "ponds",
+            diet: "mud"
+        }
+    ]
+}
